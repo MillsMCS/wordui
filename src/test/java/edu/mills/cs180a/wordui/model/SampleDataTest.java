df0be32e47e0ccede4c27d5064c852670924d531
iserrano <iserrano@mills.edu>
2020-12-01 18:37:07 -0800

    Add test for addWordOfTheDay

--- src/test/java/edu/mills/cs180a/wordui/model/SampleDataTest.java
+++ src/test/java/edu/mills/cs180a/wordui/model/SampleDataTest.java
@@ -5,6 +5,7 @@ import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
@@ -15,34 +16,42 @@ import edu.mills.cs180a.wordnik.client.api.WordApi;
 import edu.mills.cs180a.wordnik.client.api.WordsApi;
 import edu.mills.cs180a.wordnik.client.model.FrequencySummary;
 import edu.mills.cs180a.wordnik.client.model.WordOfTheDay;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 
 class SampleDataTest {
     private final WordsApi mockWordsApi = mock(WordsApi.class);
     private final WordOfTheDay mockWordOfTheDay = mock(WordOfTheDay.class);
     private final WordApi mockWordApi = mock(WordApi.class);
-    private static final Map<String, FrequencySummary> FREQ_MAP = Map.of("apple",
-            makeFrequencySummary(List.of(
+    private static final String WORD_OF_THE_DAY = "electuary";
+    private static final Map<String, FrequencySummary> FREQ_MAP = Map.of(
+            "apple", makeFrequencySummary(List.of(
                     makeMap(2000, 339),
                     makeMap(2001, 464))),
             "orange", makeFrequencySummary(List.of(
                     makeMap(2000, 774),
-                    makeMap(2001, 941))));
+                    makeMap(2001, 941))),
+            WORD_OF_THE_DAY, makeFrequencySummary(List.of(makeMap(2012,1))));
 
     private static final List<Object> DEFINITIONS = List.of(
-            "In pharmacy, a medicine composed of powders or other ingredients, incorporated with "
-                    + "some conserve, honey, or syrup, originally made in a form to be licked by the patient.",
-                    "Any preparation of a medicine mixed with honey or similar in order to make it more "
-                            + "palatable to swallow.\r\n");
+            "In pharmacy, a medicine composed of powders or other ingredients, incorporated with some conserve, honey, or syrup, originally made in a form to be licked by the patient.",
+            "Any preparation of a medicine mixed with honey or similar in order to make it more palatable to swallow.\r\n");
+
+    private static final List<Object> MOCK_DEFINITIONS_MAP =
+            List.of(Map.of("text", DEFINITIONS.get(0)));
 
     @BeforeEach
     void setup() {
         when(mockWordApi.getWordFrequency(anyString(), anyString(), anyInt(), anyInt()))
         .thenAnswer(invocation -> FREQ_MAP.get(invocation.getArgument(0)));
         when(mockWordsApi.getWordOfTheDay()).thenReturn(mockWordOfTheDay);
-        when(mockWordOfTheDay.getWord()).thenReturn("electuary");
-        when(mockWordOfTheDay.getDefinitions()).thenReturn(DEFINITIONS);
+        when(mockWordOfTheDay.getWord()).thenReturn(WORD_OF_THE_DAY);
+        when(mockWordOfTheDay.getDefinitions()).thenReturn(MOCK_DEFINITIONS_MAP);
+        //        when(mockWordApi.getWordFrequency(eq(WORD_OF_THE_DAY), anyString(), anyInt(), anyInt()))
+        //        .thenReturn(WORD_OF_THE_DAY_FREQ);
     }
 
+    //private static final FrequencySummary WORD_OF_THE_DAY_FREQ = makeFrequencySummary(List.of(makeMap(2012, 1)));
 
     private static FrequencySummary makeFrequencySummary(List<Object> freqs) {
         FrequencySummary fs = mock(FrequencySummary.class);
@@ -66,7 +75,15 @@ class SampleDataTest {
     void testGetWordOfTheDay() {
         WordOfTheDay word = SampleData.getWordOfTheDay(mockWordsApi);
         assertEquals(mockWordOfTheDay, word);
-        assertEquals("electuary", word.getWord());
-        assertEquals(2, word.getDefinitions().size());
+        assertEquals(WORD_OF_THE_DAY, word.getWord());
+        assertEquals(1, word.getDefinitions().size());
+    }
+
+    @Test
+    void testAddWordOfTheDay() {
+        List<WordRecord> testList = new ArrayList<WordRecord>();
+        ObservableList<WordRecord> observable = FXCollections.observableList(testList);
+        SampleData.addWordOfTheDay(mockWordApi, mockWordsApi, observable);
+        assertEquals(1, observable.size());
     }
 }

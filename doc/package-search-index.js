954fdc81c8bbbad0997f7810bb7263983d750f22
Makie <mmaekawa@mills.edu>
2020-12-06 16:00:30 -0800

    Delete javadoc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"edu.mills.cs180a.wordui.model"}]
--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"edu.mills.cs180a.wordui.model","l":"SampleData"}]
